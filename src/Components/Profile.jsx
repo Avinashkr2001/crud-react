2a5118caf6b586562d0263337cf78b0105445ead
Avinash <2019avinash2001@gmail.com>
2024-07-07 01:12:20 +0530

    Error Fixed in profile.jsx

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { IoCloudUploadOutline } from "react-icons/io5";
 import '../Css/Profile.css';
 
 const Profile = () => {
